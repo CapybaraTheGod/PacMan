e786c70f42bc2da1e70d47220b37269c99bebf10
kamibara <tkmt123@naver.com>
2026-03-19 23:26:22 +0900

    각 챕터를 위한 기본 세팅

--- PacMan/Game.cpp
+++ PacMan/Game.cpp
@@ -10,44 +10,6 @@ Game::Game()
 // 초기화
 bool Game::Initialize()
 {
-	int sdlResult = SDL_Init(SDL_INIT_VIDEO);
-	if (sdlResult != 0)
-	{
-		SDL_Log("SDL을 초기화 할 수 없었습니다.: %s", SDL_GetError());
-		return false;
-	}
-
-	// SDL 윈도우 생성하기
-	mWindow = SDL_CreateWindow(
-		"Game Programming in c++ (Chapter 1)", // 윈도우 제목
-		100, // 윈도우의 좌측 상단 x좌표
-		100, // 윈도우의 좌측상단 y좌표
-		1024, // 윈도우 너비
-		768, // 윈도우 높이
-		0 // 플래그 (아무런 플래그도 지정하지 않음을 의미)
-	);
-
-	if (!mWindow)
-	{
-		SDL_Log("윈도우 생성 실패: %s", SDL_GetError());
-		return false;
-	}
-
-	// 창을 생성한 후, SDL Renderer를 생성
-	mRenderer = SDL_CreateRenderer(
-		mWindow,
-		-1,
-		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-	);
-
-	// Render 실패 시
-	if (!mRenderer)
-	{
-		SDL_Log("Failed to create renderer: %s", SDL_GetError());
-		return false;
-	}
-
-	return true;
 }
 
 // 런 루프
@@ -104,16 +66,4 @@ void Game::UpdateGame()
 // 출력 생성
 void Game::GenerateOutput()
 {
-	// 파란색 지정
-	SDL_SetRenderDrawColor(
-		mRenderer,
-		0,
-		0,
-		255,
-		255
-	);
-	// 후면 버퍼 지우기
-	SDL_RenderClear(mRenderer);
-	// 전면 버퍼와 후면 버퍼를 교환
-	SDL_RenderPresent(mRenderer);
 }
